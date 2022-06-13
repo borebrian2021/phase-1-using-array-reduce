805537d3d0a3e0b5583cfb57d6e5fcab2d5becbf
Brian Kimutai <brian.kimutai@student.moringaschool.com>
2022-06-13 19:36:59 +0300

    commit tree f4dec27d39ea6480287c96f21ff5de94a9a14dfd

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
-
 // Code your solution here
+function batteries(array,reducer){
+    let totalBatteries=array.reduce(function (previousValue, currentValue) {
+        return previousValue + currentValue;
+    });
+    return totalBatteries;
+}
+batterries(batteryBatches)
